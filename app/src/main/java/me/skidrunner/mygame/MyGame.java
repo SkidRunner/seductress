a01b65ecc88e31445e53fe264b1c4d2dfbe57718
skidrunner <skidrunner@gmail.com>
2018-11-17 11:53:22 -0600

    build fix

--- app/src/main/java/me/skidrunner/mygame/MyGame.java
+++ app/src/main/java/me/skidrunner/mygame/MyGame.java
@@ -1,9 +1,17 @@
 package me.skidrunner.mygame;
 import com.jme3.app.SimpleApplication;
+import com.jme3.scene.Node;
 
 public class MyGame extends SimpleApplication {
 
 	@Override
-	public void simpleInitApp() { }
+	public void simpleInitApp() {
+		flyCam.setEnabled(false);
+		float width = getCamera().getWidth();
+		float height = getCamera().getHeight();
+		
+		Node scaledGuiNode = new Node("Scaled GUI Node");
+		
+	}
 	
 }
