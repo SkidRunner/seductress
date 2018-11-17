f28fa4b8542574164b75e4afdc543444655b1c74
skidrunner <skidrunner@gmail.com>
2018-11-17 09:01:03 -0600

    Initial commit

---
+++ app/src/main/java/me/skidrunner/mygame/MainActivity.java
@@ -0,0 +1,14 @@
+package me.skidrunner.mygame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
